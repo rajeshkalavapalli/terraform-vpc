3b36c4accc0b879809ca3073702b57c1c7a0531e
rajeshkalavapalli <139232470+rajeshkalavapalli@users.noreply.github.com>
2024-04-20 18:55:18 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,6 +1,9 @@
 output "azs" {
   value = data.aws_availability_zones.azs.names
 }
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
 
 output "public_subnet_ids" {
   value = aws_subnet.public[*].id
