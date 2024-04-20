acde615947d62428b85d8c0d17c6f263891d968c
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-04-20 15:48:37 +0530

    final data

--- test-vpc/main.tf
+++ test-vpc/main.tf
@@ -5,5 +5,16 @@ module "ROBOSHOP" {
   environment = var.environment
   common_tags = var.common_tags
   vpc_tags = var.vpc_tags
-  
+
+  # public subnets
+  public_subnets_cidr = var.public_subnets_cidr
+  #private subnets
+  private_subnets_cidr = var.private_subnets_cidr
+
+  #database 
+  database_subnets_cidr = var.database_subnets_cidr
+  #is peering require
+  is_peering_required = var.is_peering_required
+
+ 
 }
